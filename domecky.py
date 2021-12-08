3da54365d103516c6d108cc01f605e225df90239
AdamJeHot <90827236+AdamJeHot@users.noreply.github.com>
2021-12-08 13:52:30 +0100

    Add files via upload

---
+++ domecky.py
@@ -0,0 +1,23 @@
+from turtle import *
+shape('turtle')
+speed(1)
+
+left(90)
+for i in range (3):
+    forward(50)
+    right(90)
+
+right(45)
+forward(70.7)
+
+for v in range (2):
+    right(90)
+    forward(35.35)
+
+right(90)
+forward(70.7)
+
+left(135)
+forward(50)
+
+exitonclick
